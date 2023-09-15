90935749167234a0214add6c276bef887533faa7
Maddie <115709272+Maddily@users.noreply.github.com>
2023-09-15 13:36:15 +0300

    Include headers and function prototypes

--- main.h
+++ main.h
@@ -1,7 +1,14 @@
 #ifndef HEADER_H
 #define HEADER_H
 
-int _printf(const char *format, ...);
+#include <stdarg.h>
+#include <unistd.h>
 
+int _printf(const char *format, ...);
+int find_length(char *s);
+void char_format_handler(va_list ap, char character, int count);
+void str_format_handler(char *string, int count, int len, char *null);
+void percent_handler(char character, int count);
+void default_handler(char character, const char *format, int count);
 
 #endif
