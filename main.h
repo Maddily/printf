3d06603b527e97ac6bfd9f1481c0d339ee6bc163
Maddie <115709272+Maddily@users.noreply.github.com>
2023-09-17 11:05:24 +0300

    Handle 'l' and 'h' length modifiers

--- main.h
+++ main.h
@@ -18,12 +18,16 @@ void str_format_handler(char *string, int *count, int *total, char *buffer);
 void ptr_format_handler(va_list ap, int *count, int *total, char *buffer);
 void percent_handler(int *count, int *total, char *buffer);
 void default_handler(const char *format, int *count, int *total, char *buffer);
+void handle_zero_number(int *count, int *total, char *buffer);
 
 /* --------- Numbers ------*/
 void int_format_handler(int num, int *count, int *total, char *buffer);
-void u_format_handler(unsigned int number, int *count, int *total, char *buffer);
-void o_format_handler(unsigned int number, int *count, int *total, char *buffer);
-void x_format_handler(unsigned int number, int *count, int *total, char *buffer);
+void u_format_handler(unsigned int number, int *count,
+		int *total, char *buffer);
+void o_format_handler(unsigned int number, int *count,
+		int *total, char *buffer);
+void x_format_handler(unsigned int number, int *count,
+		int *total, char *buffer);
 void printUpperHex(unsigned int number, int *count, int *total, char *buffer);
 
 /* --------- Characters ----*/
@@ -32,7 +36,32 @@ int our_ptchar(char c);
 /*--- Custom Format Specifiers ---*/
 void custom_b_handler(va_list ap, int *count, int *total, char *buffer);
 void custom_S_handler(char *string, int *count, int *total, char *buffer);
-void custom_reverse_handler(char *string, int *count, int *total, char *buffer);
+void custom_reverse_handler(char *string, int *count,
+		int *total, char *buffer);
+
+/*--- Length Modifiers ---*/
+void long_modifier_handler(const char *format, va_list ap, int *count,
+		int *total, char *buffer);
+void long_int_handler(long number, int *count, int *total, char *buffer);
+void long_u_handler(unsigned long number, int *count,
+		int *total, char *buffer);
+void long_o_handler(unsigned long number, int *count,
+		int *total, char *buffer);
+void long_x_handler(unsigned long number, int *count,
+		int *total, char *buffer);
+void long_upper_x_handler(unsigned long number, int *count,
+		int *total, char *buffer);
+void short_modifier_handler(const char *format, va_list ap,
+		int *count, int *total, char *buffer);
+void short_int_handler(short number, int *count, int *total, char *buffer);
+void short_u_handler(unsigned short number, int *count,
+		int *total, char *buffer);
+void short_o_handler(unsigned short number, int *count,
+		int *total, char *buffer);
+void short_x_handler(unsigned short number, int *count,
+		int *total, char *buffer);
+void short_upper_x_handler(unsigned short number, int *count,
+		int *total, char *buffer);
 
 /*--- Error Messages ---*/
 void trailing_percent_error(const char *format);
--- u_handler.c
+++ u_handler.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-void handle_zero_number(int *count, int *total, char *buffer);
-
 /**
  * u_format_handler - handles 'u' specifier
  * @number: unsigned int
@@ -40,24 +38,5 @@ void u_format_handler(unsigned int number, int *count,
 			*total += write(1, (const void *)buffer, *count);
 			*count = 0;
 		}
-	}
-}
-
-/**
- * handle_zero_number - handles case if input is zero
- * @count: number of characters printed
- * @total: A pointer to the total number of characters printed
- * @buffer: A pointer to the buffer holding the characters to be printed
- */
-
-void handle_zero_number(int *count, int *total, char *buffer)
-{
-	buffer[*count] = '0';
-	(*count)++;
-
-	if (*count == 1024)
-	{
-		*total += write(1, (const void *)buffer, *count);
-		*count = 0;
 	}
 }
