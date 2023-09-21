e9a5c5292459661501d0b3120375d6183d93777a
Sibongile <sibongile.nhlema@gmail.com>
2023-09-21 04:53:58 -0700

    fixed betty issues on intToStr.c

--- intToStr.c
+++ intToStr.c
@@ -12,41 +12,31 @@
 
 int my_itoa(const char *format, char *buffer, int num, int base)
 {
-	int digit, len, temp;
-	int original_len;
+	int digit, len, temp, original_len;
 
 	if (buffer == NULL)
-	{
 		return (0);
-	}
-
 	temp = num;
 	len = 0;
-
 	if (num == 0)
 	{
 		buffer[0] = '0';
 		buffer[1] = '\0';
 		return (1);
 	}
-
 	if (num < 0)
 	{
 		num = -num;
 		buffer[0] = '-';
 		len++;
 	}
-
 	while (temp != 0)
 	{
 		temp /= base;
 		len++;
 	}
-
 	original_len = len;
-
 	buffer[len] = '\0';
-
 	while (num != 0)
 	{
 		digit = num % base;
@@ -55,16 +45,11 @@ int my_itoa(const char *format, char *buffer, int num, int base)
 			if (format[1] == 'X')
 				buffer[len - 1] = 'A' + digit - 10;
 			else
-				buffer[len - 1] = 'a' + digit - 10;
-		}
+				buffer[len - 1] = 'a' + digit - 10; }
 		else
-		{
 			buffer[len - 1] = '0' + digit;
-		}
-
 		num /= base;
-		len--;
-	}
+		len--; }
 	return (original_len);
 }
 
